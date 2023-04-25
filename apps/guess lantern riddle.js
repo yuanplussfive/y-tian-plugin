ba13de9648d635b6a96a864ca3577fec4b8fb255
Hello World <xuanatwm@gmail.com>
2023-04-25 17:32:42 +0000

    update apps/guess lantern riddle.js.
    Signed-off-by: Hello World <xuanatwm@gmail.com>

--- apps/guess lantern riddle.js
+++ apps/guess lantern riddle.js
@@ -1,9 +1,5 @@
-let segment = ""
-try{
-segment =(await import("oicq")).segment
-}catch(err){
-segment =(await import("icqq")).segment
-}
+import {getSegment} from "../model/segment.js"
+    const segment = await getSegment()
 import fetch from "node-fetch";
 import common from'../../../lib/common/common.js'
 const _path = process.cwd();
