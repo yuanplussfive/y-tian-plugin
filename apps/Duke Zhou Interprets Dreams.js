99418244013474ebc8d620421ff5329b739a6c6f
Hello World <xuanatwm@gmail.com>
2023-04-25 17:31:21 +0000

    update apps/Duke Zhou Interprets Dreams.js.
    Signed-off-by: Hello World <xuanatwm@gmail.com>

--- apps/Duke Zhou Interprets Dreams.js
+++ apps/Duke Zhou Interprets Dreams.js
@@ -1,6 +1,5 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import fetch from 'node-fetch'
-let segment = ""
 import {getSegment} from "../model/segment.js"
     const segment = await getSegment()
 
