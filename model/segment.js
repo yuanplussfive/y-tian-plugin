ae299731907cd8e7cd5824cf8d921f3d236ef365
Hello World <xuanatwm@gmail.com>
2023-05-07 05:26:11 +0000

    删除文件 model/segment.js

--- model/segment.js
+++
@@ -1,9 +0,0 @@
-export async function getSegment(){
-try{
-    let segment = ''
-    segment =(await import("oicq")).segment
-    }catch(err){
-    segment =(await import("icqq")).segment
-    }
-    return segment
-}
