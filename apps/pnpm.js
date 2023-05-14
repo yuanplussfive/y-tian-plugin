338bdd688729b294a9b6b257f57452ab0b0d9f99
Hello World <xuanatwm@gmail.com>
2023-05-14 13:28:00 +0000

    更新自动安装依赖脚本
    Signed-off-by: Hello World <xuanatwm@gmail.com>

---
+++ apps/pnpm.js
@@ -0,0 +1,31 @@
+﻿import fs from 'fs';
+import { exec } from 'child_process';
+import path from 'path';
+import cfg from '../../../lib/config/config.js'
+const _path = process.cwd()
+
+let dirpath = _path + '/plugins/y-tian-plugin/node_modules/request'
+let dirpath2 = _path + '/plugins/y-tian-plugin'
+
+fs.promises.access(dirpath)
+  .then(() => {
+  
+  })
+  .catch(() => { 
+    const cmd = 'pnpm install';
+    const options = {
+      cwd: dirpath2,
+    };
+    
+    exec(cmd, options, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行命令失败：${cmd}`);
+        console.error(`错误信息：${error.message}`);
+        return;
+      }
+      
+      setTimeout(() => Bot.pickFriend(cfg.masterQQ[0]).sendMsg('检测到您的阴天依赖不完整，已自动为您安装依赖，请重启云崽以生效。'), 5000)
+    
+    });
+  });
+ 
