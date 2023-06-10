1ef2cdfc38ab83604f326f46e6b34563b9096d00
yuanpluss <2166683295@qq.com>
2023-06-10 17:59:08 +0000

    更新加载
    Signed-off-by: yuanpluss <2166683295@qq.com>

---
+++ index.js
@@ -0,0 +1,39 @@
+import fs from 'node:fs'
+import chalk from 'chalk'
+
+const files = fs.readdirSync('./plugins/y-tian-plugin/apps').filter(file => file.endsWith('.js'))
+
+let ret = []
+
+logger.info(chalk.rgb(50, 240, 108)(`---~~~~·❤·~~~~---`))
+logger.info(chalk.white(`       ☁️☁️☁️☁️☁️☁️☁️`)+chalk.white(`⚡    ⚡`)+chalk.white(`  ⚡⚡⚡⚡⚡`))
+logger.info(chalk.blue(`        💧💧💧`)
++chalk.white(`  ⚡⚡`)+chalk.white(`        ⚡`))
+logger.info(chalk.white(`     ☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️`)
++chalk.white(` ⚡`)+chalk.white(`         ⚡`))
+logger.info(chalk.blue(`     💧💧💧💧💧`)
++chalk.white(`  ⚡ `)+chalk.white(`        ⚡`))
+logger.info(chalk.white(`☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️☁️`))
+
+logger.info(chalk.rgb(50, 240, 108)(`阴天插件Y-Tian-plugin已装载完毕`))
+logger.info(chalk.rgb(50, 240, 108)(`作者-鸢 & 天球生物`))
+logger.info(chalk.rgb(50, 240, 108)(`---------------------`));
+ 
+files.forEach((file) => {
+  ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
+let apps = {}
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+  }
+  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+}
+export { apps }
