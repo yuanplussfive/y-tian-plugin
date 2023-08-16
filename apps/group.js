93e15f346c7b1aa117bfe914d246b02d740fb0f9
yuanpluss <2166683295@qq.com>
2023-08-16 00:56:28 +0000

    update apps/group.js.
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- apps/group.js
+++ apps/group.js
@@ -52,7 +52,7 @@ export class KeyWordsWithDraw extends plugin {
             /** 功能描述 */
             dsc: '简单开发示例',
             event: 'message.group',
-            priority: 300000,
+            priority: 50000,
             rule: [{
           /** 命令正则匹配 */
           reg:"^#?addban(.*)$",
