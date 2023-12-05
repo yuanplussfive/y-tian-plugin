16c283107cb09a85e3494fc3683307a4d59c6659
yuanpluss <2166683295@qq.com>
2023-12-05 10:52:06 +0000

    更新😔
    Signed-off-by: yuanpluss <2166683295@qq.com>

---
+++ model/Anime_tts.js
@@ -0,0 +1,30 @@
+export function Anime_tts(speakers, answer){
+let record = `https://genshinvoice.top/api?speaker=${speakers}&text=${answer}&format=wav&language=ZH&length=1&sdp=0.4&noise=0.6&noisew=0.8`
+return record
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
