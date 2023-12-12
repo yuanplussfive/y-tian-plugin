3e751633af462c3e68db109efe35169ca00a7c75
yuanpluss <2166683295@qq.com>
2023-12-12 13:05:44 +0000

    修复并优化tts语音
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- model/Anime_tts.js
+++ model/Anime_tts.js
@@ -1,27 +1,29 @@
 import fetch from 'node-fetch';
 import yaml from 'yaml'
 import fs from "fs"
+import os from "os"
 const _path = process.cwd()
 
 async function Anime_tts(speakers, answer) {
-const yamlStr = fs.readFileSync(_path+'/data/YTtts_Setting/Setting.yaml', 'utf8');
-const config = yaml.parse(yamlStr);
-const session_hash = Math.random().toString(36).substring(2);
+  const yamlStr = fs.readFileSync(_path+'/data/YTtts_Setting/Setting.yaml', 'utf8');
+  const config = yaml.parse(yamlStr);
+  const session_hash = Math.random().toString(36).substring(2);
 
- const body = {
+  const body = {
     "data": [
       answer,
       speakers,
-      config.motion,
       config.sdp_ratio,
       config.noise,
       config.noise_w,
       config.length,
       "ZH",
-      null
+      null,
+      "Happy",
+      "Text prompt"
     ],
     "event_data": null,
-    "fn_index": 2,
+    "fn_index": 0,
     "session_hash": session_hash
   };
 
@@ -46,14 +48,53 @@ const session_hash = Math.random().toString(36).substring(2);
       record_name = "https://v2.genshinvoice.top/file=" + record_name;
       resolve(record_name);
     })
-    .catch(error => {
+    .catch(async error => {
       console.error('Error:', error);
-      reject(error);
+      let address = await get_ip_address()
+      let body = {
+        "voice_id": 111,
+        "text": answer,
+        "format": "mp3", 
+        "voice_speed": "0%",
+        "rate": "1.1",
+        "client_ip": address
+      }
+      let response = await fetch("https://api.ttson.cn:50602/flashsummary/tts", {
+        "headers": {
+          "content-type": "application/json",
+          "Referer": "https://www.ttson.cn/"
+        },
+        "body": JSON.stringify(body),
+        "method": "POST"
+      });
+      let res = await response.json()
+      console.log(res)
+      let voice_path = await res.voice_path
+      let url = `https://api.ttson.cn:50602/flashsummary/retrieveFileData?user_id=user1&task_uuid=dx2oxaoa&task_type=0&download_type=0&file_name=input.mp4&voice_audio_path=${voice_path}`
+      resolve(url);
     });
   });
 }
 
-export { Anime_tts };
+async function get_ip_address(){
+  const interfaces = os.networkInterfaces();
+  for (let interfaceName in interfaces) {
+    const interfaceInfo = interfaces[interfaceName];
+    for (let info of interfaceInfo) {
+      if (info.family === 'IPv4' && !info.internal) {
+        return info.address;
+      }
+    }
+  }
+  return "";
+}
+
+export { Anime_tts }
+
+
+
+
+
 
 
 
