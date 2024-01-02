bae91958d109c82c627af01b1298972f5dc55a3b
yuanpluss <2166683295@qq.com>
2024-01-02 20:07:08 +0000

    update YTOpen-Ai/other-models.js.
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- YTOpen-Ai/other-models.js
+++ YTOpen-Ai/other-models.js
@@ -13,87 +13,110 @@ export const other_models = [
   },
   {
     "id":3,
+    "model": "gpt-3.5-turbo-online",
+    "points": "OpenAi",
+    "token": 16384
+  },
+  {
+    "id":4,
     "model": "gpt-4",
     "points": "OpenAi",
     "token": 8192
   },  
   {
-    "id":4,
+    "id":5,
     "model": "gpt-4-1106-preview",
     "points": "OpenAi",
     "token": 131072
   },
   {
-    "id":5,
+    "id":6,
     "model": "search",
     "points": "google/duckduckgo/baidu",
     "token": "∝"
   },
   {
-    "id":6,
+    "id":7,
+    "model": "pi",
+    "points": "pi",
+    "token": 2048
+  },
+  {
+    "id":8,
+    "model": "google-bard",
+    "points": "google",
+    "token": 8000
+  },
+  {
+    "id":9,
     "model": "claude-1.3-100k",
     "points": "Claude",
     "token": 100000
   },
   {
-    "id":7,
+    "id":10,
     "model": "claude-2",
     "points": "Claude",
     "token": 100000
   },
   {
-    "id":8,
+    "id":11,
+    "model": "claude-2-sillytavern",
+    "points": "Claude+sillytavern",
+    "token": 100000
+  },
+  {
+    "id":12,
     "model": "chatglm-turbo",
     "points": "清华&智谱",
     "token": 8000
   },
   {
-    "id":9,
+    "id":13,
     "model": "chatglm-pro",
     "points": "清华&智谱",
     "token": 8000
   },
   {
-    "id":10,
+    "id":14,
     "model": "meta-llama",
     "points": "llama",
     "token": "70000"
   },
   {
-    "id":11,
+    "id":15,
     "model": "mistralai",
     "points": "Mistral",
     "token": "8500"
   },
   {
-    "id":12,
+    "id":16,
+    "model": "mistral-34b",
+    "points": "Mistral",
+    "token": 34000
+  },
+  {
+    "id":17,
     "model": "codellama",
     "points": "Microsoft",
     "token": "34000"
   },
   {
-    "id":13,
+    "id":18,
     "model": "jondurbin",
     "points": "airoboros",
     "token": "70000"
   },
   {
-    "id":14,
+    "id":19,
     "model": "ownthink",
     "points": "sizhi",
     "token": "∝"
   },
   {
-    "id":15,
+    "id":20,
     "model": "qingyunke",
     "points": "qingyunke",
     "token": "∝"
   }
 ]
-
-
-
-
-
-
-
