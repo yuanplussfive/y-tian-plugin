15311c030b049a1d598123c73f5450ce460b9b5d
yuanpluss <2166683295@qq.com>
2024-03-17 11:59:39 +0000

    优化
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- YTOpen-Ai/god-conversation.js
+++ YTOpen-Ai/god-conversation.js
@@ -31,7 +31,10 @@ async function god_conversation(imgurl, dirpath, e, apiurl, group, common, puppe
      source = "undefined"
    }
  console.log(source.raw_message)
-      if ((e?.message.find(val => val.type === 'image') && e?.msg) || (source && source?.raw_message && (source?.raw_message?.includes('[图片]') || source?.raw_message?.includes('[动画表情]'))) || (e?.file && e?.isPrivate)) {    
+    let aiSettingsPath = _path + '/data/YTAi_Setting/data.json';
+    let aiSettings = JSON.parse(await fs.promises.readFile(aiSettingsPath, "utf-8"));
+    let { ai_private_plan, ai_private_open } = aiSettings.chatgpt;
+      if ((e?.message.find(val => val.type === 'image') && e?.msg) || (source && source?.raw_message && (source?.raw_message?.includes('[图片]') || source?.raw_message?.includes('[动画表情]'))) || (e?.file && e?.isPrivate && ai_private_plan === "god" && ai_private_open === true)) {    
       if (model == "gpt-4-all" || model == "gpt-4-dalle" || model == "gpt-4-v" || model == "gemini-pro-vision" || model == "claude-3-opus-20240229" || model == "claude-3-sonnet-20240229" || model == "claude-3-haiku-20240307") {
        message = await handleMsg(e, msg)
         msg = [ 
@@ -58,7 +61,12 @@ console.log(msg)
     await saveUserHistory(userid, history);
 
 async function formatMessage(originalMsg) {
-    return originalMsg.replace(/\/chat|#chat/g, "").trim().replace(new RegExp(Bot_Name, "g"), "");
+    if (originalMsg) {
+    const msgs = originalMsg.replace(/\/godgpt|#chat/g, "").trim().replace(new RegExp(Bot_Name, "g"), "");
+    return msgs
+  } else {
+  return undefined
+ }
 }
 
 async function loadUserHistory(userId) {
