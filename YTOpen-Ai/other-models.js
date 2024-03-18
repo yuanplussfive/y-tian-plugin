091656cf85f6e26a5319c0173b78539f22d2c86f
yuanpluss <2166683295@qq.com>
2024-03-18 18:48:04 +0000

    优化附加模型
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- YTOpen-Ai/other-models.js
+++ YTOpen-Ai/other-models.js
@@ -28,77 +28,95 @@ export const other_models = [
     "model": "gpt-4-1106-preview",
     "points": "OpenAi",
     "token": 131072
-  },  
-  {
-    "id":6,
-    "model": "gpt-4-plugins",
-    "points": "OpenAi",
-    "token": 131072
   },
   {
-    "id":7,
+    "id":6,
     "model": "search",
     "points": "google/duckduckgo/baidu",
     "token": "∝"
   },
   {
-    "id":8,
+    "id":7,
     "model": "claude-1.3-100k",
     "points": "Claude",
     "token": 100000
   },
   {
-    "id":9,
+    "id":8,
     "model": "claude-2",
     "points": "Claude",
     "token": 200000
   },
   {
+    "id":9,
+    "model": "claude-3-haiku",
+    "points": "Claude",
+    "token": 200000
+  },
+  {
     "id":10,
-    "model": "claude-2-sillytavern",
-    "points": "Claude+sillytavern",
+    "model": "claude-3-sonnect",
+    "points": "Claude",
     "token": 200000
   },
   {
     "id":11,
+    "model": "gemini-pro",
+    "points": "Google",
+    "token": 8000
+  },
+  {
+    "id":12,
     "model": "chatglm-turbo",
     "points": "清华&智谱",
     "token": 8000
   },
   {
-    "id":12,
+    "id":13,
     "model": "chatglm-pro",
     "points": "清华&智谱",
     "token": 8000
   },
   {
-    "id":13,
+    "id":14,
     "model": "chatglm-4",
     "points": "清华&智谱",
     "token": 130000
   },
   {
-    "id":14,
+    "id":15,
     "model": "meta-llama",
     "points": "llama",
     "token": "70000"
   },
   {
-    "id":15,
-    "model": "mistralai",
+    "id":16,
+    "model": "mistral",
     "points": "Mistral",
     "token": "8500"
   },
   {
-    "id":16,
+    "id":17,
+    "model": "mistral-large",
+    "points": "Mistral",
+    "token": "8500"
+  },
+  {
+    "id":18,
     "model": "codellama",
     "points": "Microsoft",
     "token": "34000"
   },
   {
-    "id":17,
+    "id":19,
     "model": "jondurbin",
     "points": "airoboros",
     "token": "70000"
+  },
+  {
+    "id":20,
+    "model": "moonshot-v1-128k",
+    "points": "moonshot",
+    "token": "128000"
   }
 ]
