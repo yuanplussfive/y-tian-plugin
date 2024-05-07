2c655efc42fe036bf096bd63043752f9dcd94d7b
yuanpluss <2166683295@qq.com>
2024-05-07 14:41:24 +0000

    函数向量优化
    Signed-off-by: yuanpluss <2166683295@qq.com>

--- YTOpen-Ai/god-models.js
+++ YTOpen-Ai/god-models.js
@@ -1,207 +1,479 @@
 export const god_models = [
-  {
-    "model": "gpt-3.5-turbo",
-    "points": 10,
-    "token": 4096
-  },
-  {
-    "model": "gpt-3.5-turbo-16k",
-    "points": 10,
-    "token": 16384
-  },
-  {
-    "model": "gpt-4",
-    "points": 750,
-    "token": 8192
-  },
-  {
-    "model": "gpt-4-32k",
-    "points": 1200,
-    "token": 32768
-  },
-  {
-    "model": "gpt-4-all",
-    "points": 1150,
-    "token": 32768
-  },
-  {
-    "model": "gpt-4-v",
-    "points": 1150,
-    "token": 32768
-  },
-  {
-    "model": "gpt-4-1106-preview",
-    "points": 1200,
-    "token": 128000
-  },
-  {
-    "model": "gpt-4-0125-preview",
-    "points": 1200,
-    "token": 128000
-  },
-  {
-    "model": "gpt-4-turbo-preview",
-    "points": 1200,
-    "token": 128000
-  },
-  {
-    "model": "claude-1-100k",
-    "points": 20,
-    "token": 100000
-  },
-  {
-    "model": "claude-1.3",
-    "points": 20,
-    "token": 7000
-  },
-  {
-    "model": "claude-1.3-100k",
-    "points": 20,
-    "token": 100000
-  },
-  {
-    "model": "claude-2",
-    "points": 20,
-    "token": 200000
-  },
-  {
-    "model": "claude-3-haiku-20240307",
-    "points": 0.03,
-    "token": 200000
-  },
-  {
-    "model": "claude-3-sonnet-20240229",
-    "points": 0.3,
-    "token": 200000
-  },
-  {
-    "model": "claude-3-opus-20240229",
-    "points": 1,
-    "token": 200000
-  },
-  {
-    "model": "google-palm",
-    "points": 10,
-    "token": 4096
-  },
-  {
-    "model": "llama-2-7b",
-    "points": 10,
-    "token": 7000
-  },
-  {
-    "model": "llama-2-13b",
-    "points": 10,
-    "token": 13000
-  },
-  {
-    "model": "llama-2-70b",
-    "points": 10,
-    "token": 70000
-  },
-  {
-    "model": "code-llama-7b",
-    "points": 10,
-    "token": 7000
-  },
-  {
-    "model": "code-llama-13b",
-    "points": 10,
-    "token": 13000
-  },
-  {
-    "model": "code-llama-34b",
-    "points": 10,
-    "token": 34000
-  },
-  {
-    "model": "gemini-pro",
-    "points": 200,
-    "token": 8000
-  },
-  {
-    "model": "gemini-pro-vision",
-    "points": 200,
-    "token": 8000
-  },
-  {
-    "model": "qwen-72b",
-    "points": 100,
-    "token": 72000
-  },
-  {
-    "model": "mixtral-8x7b",
-    "points": 50,
-    "token": 8000
-  },
-  {
-    "model": "mistral-medium",
-    "points": 100,
-    "token": 32000
-  },
-  {
-    "model": "sonar-small-online",
-    "points": 10,
-    "token": 8000
-  },
-  {
-    "model": "sonar-medium-online",
-    "points": 10,
-    "token": 32000
-  },
-  {
-    "model": "sonar-small-chat",
-    "points": 10,
-    "token": 8000
-  },
-  {
-    "model": "sonar-medium-chat",
-    "points": 10,
-    "token": 32000
-  },
-  {
-    "model": "dbrx-instruct",
-    "points": 10,
-    "token": 32000
-  },
-  {
-    "model": "codellama-70b-instruct",
-    "points": 10,
-    "token": 70000
-  },
-  {
-    "model": "mistral-7b-instruct",
-    "points": 10,
-    "token": 7000
-  },
-  {
-    "model": "llava-v1.5-7b-wrapper",
-    "points": 10,
-    "token": 7000
-  },
-  {
-    "model": "llava-v1.6-34b",
-    "points": 10,
-    "token": 34000
-  },
-  {
-    "model": "mixtral-8x7b-instruct",
-    "points": 10,
-    "token": 7000
-  },
-  {
-    "model": "mixtral-8x22b",
-    "points": 10,
-    "token": 22000
-  },
-  {
-    "model": "gemma-2b-it",
-    "points": 10,
-    "token": 2000
-  },
-  {
-    "model": "gemma-7b-it",
-    "points": 10,
-    "token": 7000
-  }
+    {
+        "id": 1,
+        "model": "gpt-3.5-turbo",
+        "created": 1686935002,
+        "token": 5000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 2,
+        "model": "gpt-3.5-turbo-16k",
+        "created": 1686935002,
+        "token": 16000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 3,
+        "model": "gpt-4",
+        "created": 1686935002,
+        "token": 8000,
+        "points": 750,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 4,
+        "model": "gpt-4-1106-preview",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 5,
+        "model": "gpt-4-0125-preview",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 6,
+        "model": "gpt-4-turbo",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 7,
+        "model": "gpt-4-turbo-2024-04-09",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 8,
+        "model": "gpt-4-turbo-preview",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 9,
+        "model": "gpt-4-32k",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 10,
+        "model": "gpt-4-v",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1150,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 11,
+        "model": "gpt-4-vision-preview",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1200,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 12,
+        "model": "gpt-4-dalle",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1150,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 13,
+        "model": "gpt-4-all",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1150,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 14,
+        "model": "glm-4",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 1,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 15,
+        "model": "glm-4v",
+        "created": 1686935002,
+        "token": 2000,
+        "points": 1,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 16,
+        "model": "glm-3-turbo",
+        "created": 1686935002,
+        "token": 128000,
+        "points": 50,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 17,
+        "model": "deepseek-chat",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 0.02,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 18,
+        "model": "deepseek-coder",
+        "created": 1686935002,
+        "token": 16000,
+        "points": 0.02,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 19,
+        "model": "gemini-pro",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 100,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 20,
+        "model": "gemini-pro-vision",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 100,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 21,
+        "model": "claude-1-100k",
+        "created": 1686935002,
+        "token": 100000,
+        "points": 20,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 22,
+        "model": "claude-1.3",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 23,
+        "model": "claude-1.3-100k",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 24,
+        "model": "claude-2",
+        "created": 1686935002,
+        "token": 100000,
+        "points": 1,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 25,
+        "model": "claude-3-opus-20240229",
+        "created": 1686935002,
+        "token": 200000,
+        "points": 1,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 26,
+        "model": "claude-3-sonnet-20240229",
+        "created": 1686935002,
+        "token": 200000,
+        "points": 0.3,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 27,
+        "model": "claude-3-haiku-20240307",
+        "created": 1686935002,
+        "token": 200000,
+        "points": 0.03,
+        "style": "token",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 28,
+        "model": "google-palm",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 29,
+        "model": "llama-2-70b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 30,
+        "model": "llama-2-13b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 31,
+        "model": "llama-2-7b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 32,
+        "model": "code-llama-34b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 33,
+        "model": "code-llama-13b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 34,
+        "model": "code-llama-7b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 35,
+        "model": "stable-diffusion",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 36,
+        "model": "qwen-72b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 37,
+        "model": "mixtral-8x7b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 50,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 38,
+        "model": "mistral-medium",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 100,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 39,
+        "model": "llama-3-sonar-small-32k-online",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 40,
+        "model": "llama-3-sonar-small-32k-chat",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 41,
+        "model": "llama-3-sonar-large-32k-online",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 20,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 42,
+        "model": "llama-3-sonar-large-32k-chat",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 20,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 43,
+        "model": "dbrx-instruct",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 44,
+        "model": "codellama-70b-instruct",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 45,
+        "model": "mistral-7b-instruct",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 46,
+        "model": "llava-v1.5-7b-wrapper",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 47,
+        "model": "llava-v1.6-34b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 48,
+        "model": "mixtral-8x7b-instruct",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 49,
+        "model": "mixtral-8x22b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 50,
+        "model": "gemma-2b-it",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 51,
+        "model": "gemma-7b-it",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 10,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 52,
+        "model": "llama-3-8b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 50,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    },
+    {
+        "id": 53,
+        "model": "llama-3-70b",
+        "created": 1686935002,
+        "token": 32000,
+        "points": 50,
+        "style": "once",
+        "owned_by": "y-tian-plugin"
+    }
 ]
