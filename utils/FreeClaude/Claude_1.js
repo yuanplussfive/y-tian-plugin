7fcdb1ccb1d6d7d47d93b44698974e37ff8a0d72
yuanplussfive <2166683295@qq.com>
2024-06-29 11:22:31 +0800

    support claude35 and gemini35 system prompts for other plans

--- utils/FreeClaude/Claude_1.js
+++ utils/FreeClaude/Claude_1.js
@@ -1,26 +1,54 @@
 async function FreeClaude_1(messages, fetch, crypto) {
- try {
-  const response = await fetch("https://aliyun.zaiwen.top/admin/chatbot", {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-      "token": "null",
-      "Referer": "https://www.zaiwen.top/"
-    },
-    body: JSON.stringify({
-      "message": messages, 
-      "mode": "claude_2_1_bamboo", 
-      "key": null
-     })
-    });
-     if (!response.ok) {
-     return null
+  let previousRole = '';
+  const result = [];
+  messages.forEach((item) => {
+    if (item.role === "system") {
+      item.role = "user";
+    }
+    if (item.role === previousRole) {
+      const lastItem = result[result.length - 1];
+      lastItem.content += item.content;
     } else {
-    return await response.text();
-   } 
-  } catch { 
-  return null 
- }
+      result.push(item);
+      previousRole = item.role;
+    }
+  });
+  const url = "https://free.jgk-blog.tech/v1/messages";
+  const headers = {
+    "anthropic-version": "2023-06-01",
+    "authorization": "Bearer sk-xxxxx",
+    "content-type": "application/json",
+    "x-api-key": "sk-xxxxx",
+    "Referer": "https://app.nextchat.dev/"
+  };
+  const body = JSON.stringify({
+    messages: result,
+    stream: false,
+    model: "claude-3.5-sonnet-20240620",
+    max_tokens: 4000,
+    temperature: 0.5,
+    top_p: 1,
+    top_k: 5
+  });
+
+  for (let attempt = 0; attempt < 2; attempt++) {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body
+      });
+      const data = await response.json();
+      console.log(data)
+      if (data?.content?.[0]?.text) {
+        return data.content[0].text;
+      }
+    } catch (error) {
+      if (attempt === 1) {
+        return null;
+      }
+    }
+  }
 }
 
 export { FreeClaude_1 }
