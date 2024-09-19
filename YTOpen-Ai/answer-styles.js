c1111f992f42cd406c4d2607b98e4e8651d34514
yuanplussfive <2166683295@qq.com>
2024-09-19 10:22:37 +0800

    add more

--- YTOpen-Ai/answer-styles.js
+++ YTOpen-Ai/answer-styles.js
@@ -31,17 +31,27 @@ async function replyBasedOnStyle(styles, answer, e, model, puppeteer, fs, _path,
                 break;
             case "picture":
                 let tplFile = _path + "/plugins/y-tian-plugin/resources/html/gptx.html"
+                let dz = `${_path}/plugins/y-tian-plugin/resources/css/gptx.css`
                 const styles = JSON.parse(fs.readFileSync(_path + '/data/YTAi_Setting/data.json')).chatgpt.pictureStyles
                 if (styles) {
                     tplFile = _path + "/plugins/y-tian-plugin/resources/html/gptx2.html"
+                    dz = `${_path}/plugins/y-tian-plugin/resources/css/gptx2.css`
                 }
                 let data = {
-                    tplFile: tplFile,
-                    dz: `${_path}/plugins/y-tian-plugin/resources/css/gptx.css`,
+                    dz,
+                    tplFile,
+                    atom_one_dark_min_css: `${_path}/plugins/y-tian-plugin/resources/css/atom-one-dark.min.css`,
+                    all_min_css: `${_path}/plugins/y-tian-plugin/resources/css/all.min.css`,
+                    marked_min_js: `${_path}/plugins/y-tian-plugin/resources/js/marked.min.js`,
+                    purify_min_js: `${_path}/plugins/y-tian-plugin/resources/js/purify.min.js`,
+                    katex_css: `${_path}/plugins/y-tian-plugin/resources/css/katex.min.css`,
+                    katex_js: `${_path}/plugins/y-tian-plugin/resources/js/katex.min.js`,
+                    auto_render_js: `${_path}/plugins/y-tian-plugin/resources/js/auto-render.min.js`,
                     showdown: `${_path}/plugins/y-tian-plugin/resources/js/showdown.js`,
                     highlight: `${_path}/plugins/y-tian-plugin/resources/js/highlight.js`,
                     MathJax: `${_path}/plugins/y-tian-plugin/resources/js/MathJax.js`,
                     atom: `${_path}/plugins/y-tian-plugin/resources/css/atom.css`,
+                    highlight_min_js: `${_path}/plugins/y-tian-plugin/resources/js/highlight.min.js`,
                     MSG: msg,
                     CONTENT: answer,
                     model: model,
@@ -50,6 +60,7 @@ async function replyBasedOnStyle(styles, answer, e, model, puppeteer, fs, _path,
                     name: e.sender.nickname,
                     name1: Bot.nickname
                 };
+                console.log(data);
                 if (words < 1200) {
                     let img = await puppeteer.screenshot("777", data);
                     e.reply(img);
